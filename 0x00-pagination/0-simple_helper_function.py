72aadca4cc486f4f2ce8da50694d6848843c1d21
harper71 <princeazuka99@gmail.com>
2024-10-27 21:12:01 +0000

    commit

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -15,7 +15,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
 
     start_index: int = (page - 1) * page_size
-    end_index:int = start_index + page_size
+    end_index: int = start_index + page_size
 
     startNendIndex: Tuple[int, int] = (start_index, end_index)
     return startNendIndex
